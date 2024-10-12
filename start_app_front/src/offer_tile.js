9d6745f50b484541cfedeef6ea7583ba031f68cc
mikalaytsepl <kolyatepl@gmail.com>
2024-10-12 23:39:10 +0200

    animation for offer tile elaboration added click outside version1

--- start_app_front/src/offer_tile.js
+++ start_app_front/src/offer_tile.js
@@ -1,17 +1,17 @@
 import React, { useState } from 'react';
+import useOnclickOutside from 'react-cool-onclickoutside';
 
 const OfferTile = ({ pic, jobTitle, jobRequirements, onElaborateClick, noViewClick }) => {
     const [showDetails, setShowDetails] = useState(false);
 
     const handleElaborateClick = () => {
-        setShowDetails(showDetails => true);
+        setShowDetails(true);
         if (onElaborateClick) onElaborateClick();
     };
 
-    const closeElaborate = () => {
-        setShowDetails(showDetails => false);
-        if (onElaborateClick) onElaborateClick();
-    };
+    const ref = useOnclickOutside(() => {
+        setShowDetails(false);
+    });
 
     return (
         <div className="offer-tile-container relative mb-4 w-full">
@@ -46,6 +46,7 @@ const OfferTile = ({ pic, jobTitle, jobRequirements, onElaborateClick, noViewCli
             </div>
             {/* Detailed View Tile */}
             <div
+                ref={ref} 
                 className={`detailed-tile p-4 border shadow-lg h-[300px] rounded-xl w-full bg-gray-100 transition-transform duration-500 ease-in-out absolute top-full left-0 ${showDetails ? 'translate-y-[-100%] opacity-100' : 'translate-y-0 opacity-0'}`}
                 style={{ transition: 'transform 0.5s ease-in-out, opacity 0.5s ease-in-out' }}
             >
@@ -53,13 +54,6 @@ const OfferTile = ({ pic, jobTitle, jobRequirements, onElaborateClick, noViewCli
                 <p className="mt-2">More details about the job, company, and other requirements go here.</p>
                 {/* Additional content for detailed view */}
                 <div className="mt-4 pl-5 ottom-2 absolute bottom-4 left-0 right-0 flex justify-left">
-                    <button
-                        onClick={closeElaborate}
-                        className="btn mr-2 p-2"
-                        style={{ backgroundColor: '#044FF1', borderRadius: '50px', color: 'white' }}
-                    >
-                        Close
-                    </button>
                 </div>
             </div>
         </div>
